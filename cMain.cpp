7a450dc39049c02fd3feaf3c150b667a97180940
Bruno Cabral <cabral.brc@gmail.com>
2023-05-01 14:15:38 -0700

    start position and size

--- cMain.cpp
+++ cMain.cpp
@@ -1,6 +1,6 @@
 #include "cMain.h"
 
-cMain::cMain() : wxFrame(nullptr, wxID_ANY, "wxWidgets Video")
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "wxWidgets Video", wxPoint(30, 30), wxSize(800, 600))
 {
 }
 
