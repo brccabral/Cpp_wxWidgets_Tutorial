91863ff87d6d31d3dec4bd0bcf9427f3f1dc8d2d
Bruno Cabral <cabral.brc@gmail.com>
2023-05-01 14:19:35 -0700

    added some components

--- cMain.cpp
+++ cMain.cpp
@@ -2,6 +2,9 @@
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "wxWidgets Video", wxPoint(30, 30), wxSize(800, 600))
 {
+    m_btn1 = new wxButton(this, wxID_ANY, "Click me", wxPoint(10, 10), wxSize(150, 50));
+    m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
+    m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
 }
 
 cMain::~cMain()
--- cMain.h
+++ cMain.h
@@ -7,4 +7,9 @@ class cMain : public wxFrame
 public:
     cMain();
     ~cMain();
+
+public:
+    wxButton *m_btn1 = nullptr;
+    wxTextCtrl *m_txt1 = nullptr;
+    wxListBox *m_list1 = nullptr;
 };
